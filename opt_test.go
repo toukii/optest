7e455c72755d90d12902256d1bccffe89974ae28
shiyongbin <shiyongbin@ezbuy.com>
2019-03-28 11:25:41 +0800

    测试

--- opt_test.go
+++ opt_test.go
@@ -6,38 +6,35 @@ import (
 	"git.ezbuy.me/ezbuy/base/misc/log"
 )
 
-// func TestOpt(t *testing.T) {
-// 	Init()
-// 	vm := make(map[int64]*V, len(vs))
-// 	for _, v := range vs {
-// 		vm[v.Id] = v
-// 	}
-
-// 	road := Search(As[0], As, vm)
-// 	aids, vids, r := road.AIds, road.VIds, road.Reduce
-// 	t.Logf("aids:%+v, vids:%+v, reduce:%d, length:%d", aids, vids, r, length)
-// 	log.JSON(As)
-
-// 	Init()
-// 	road = Search(As[1], As, vm)
-// 	aids, vids, r = road.AIds, road.VIds, road.Reduce
-// 	t.Logf("aids:%+v, vids:%+v, reduce:%d, length:%d", aids, vids, r, length)
-// 	log.JSON(As)
-
-// 	Init()
-// 	road = Search(As[2], As, vm)
-// 	aids, vids, r = road.AIds, road.VIds, road.Reduce
-// 	t.Logf("aids:%+v, vids:%+v, reduce:%d, length:%d", aids, vids, r, length)
-// 	log.JSON(As)
-// }
-
-func TestA(*testing.T) {
+func TestOpt(t *testing.T) {
 	Init()
+	vm := make(map[int64]*V, len(vs))
+	for _, v := range vs {
+		vm[v.Id] = v
+	}
+
+	road := Search(As[0], As, vm)
+	t.Logf("aids:%+v, vids:%+v, reduce:%d, length:%d", road.AIds, road.VIds, road.Reduce, length)
+
+	Init()
+	road = Search(As[1], As, vm)
+	t.Logf("aids:%+v, vids:%+v, reduce:%d, length:%d", road.AIds, road.VIds, road.Reduce, length)
+
+	Init()
+	road = Search(As[2], As, vm)
+	t.Logf("aids:%+v, vids:%+v, reduce:%d, length:%d", road.AIds, road.VIds, road.Reduce, length)
+}
+
+func TestA(t *testing.T) {
+	Init()
+
 	road := SearchOpt(As, vs)
 	log.JSON(road)
+	t.Logf("length:%d", length)
 
-	// road1 := SearchOpt(As, vs)
-	// log.JSON(road1)
+	road1 := SearchOpt(As, vs)
+	log.JSON(road1)
+	t.Logf("length:%d", length)
 }
 
 func BenchmarkOptest(b *testing.B) {
